f2448aab0f48eb79a8ed177c45a7624fe239eb33
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-08-21 19:04:09 +0200

    vite use alias too

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+import { fileURLToPath } from "node:url"
 import { defineConfig } from "vite"
 import solid from "vite-plugin-solid"
 import solidDevTools from "solid-devtools/vite"
 
 export default defineConfig({
   plugins: [solidDevTools(), solid()],
+  resolve: {
+    alias: [{ find: /^~\//, replacement: fileURLToPath(new URL("./src/", import.meta.url)) }],
+  },
   server: {
     port: 8778,
   },
